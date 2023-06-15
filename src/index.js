28eeec39774aa4d439570800a9ac22ea58d7068c
marmooo <marmooo@users.noreply.github.com>
2023-06-16 01:59:09 +0900

    improve UI

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const playPanel = document.getElementById("playPanel");
+const infoPanel = document.getElementById("infoPanel");
 const countPanel = document.getElementById("countPanel");
 const scorePanel = document.getElementById("scorePanel");
 const gameTime = 120;
@@ -167,6 +168,7 @@ let countdownTimer;
 function countdown() {
   clearTimeout(countdownTimer);
   countPanel.classList.remove("d-none");
+  infoPanel.classList.add("d-none");
   playPanel.classList.add("d-none");
   scorePanel.classList.add("d-none");
   const counter = document.getElementById("counter");
@@ -180,6 +182,7 @@ function countdown() {
     } else {
       clearTimeout(countdownTimer);
       countPanel.classList.add("d-none");
+      infoPanel.classList.remove("d-none");
       playPanel.classList.remove("d-none");
       correctCount = problemCount = 0;
       document.getElementById("score").textContent = correctCount;
