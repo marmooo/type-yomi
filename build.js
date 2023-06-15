dbbbb32cb5f2a3bb1c34a284c9228c5867f498bc
marmooo <marmooo@users.noreply.github.com>
2023-06-16 01:59:05 +0900

    bump yomi-dict from 0.1.3 to 0.1.4

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 import { readLines } from "https://deno.land/std/io/mod.ts";
-import { YomiDict } from "https://raw.githubusercontent.com/marmooo/yomi-dict/v0.1.3/mod.js";
+import { YomiDict } from "https://raw.githubusercontent.com/marmooo/yomi-dict/v0.1.4/mod.js";
 
 async function getGradedWords(filepath, threshold) {
   const examples = [];
