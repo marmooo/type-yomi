1b6ca18881a20832c1370fd259738bb41f6c33c2
marmooo <marmooo@users.noreply.github.com>
2025-01-07 09:21:47 +0900

    bump yomi-dict from 0.1.8 to 0.2.0

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 import { TextLineStream } from "jsr:@std/streams/text-line-stream";
-import { YomiDict } from "npm:yomi-dict@0.1.8";
+import { YomiDict } from "npm:yomi-dict@0.2.0";
 
 async function getGradedWords(filePath, threshold) {
   const examples = [];
